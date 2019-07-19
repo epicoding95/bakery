4ba88de2b202719f734de934155cd40057f14083
Student <example@example.com>
2019-07-19 10:47:41 -0700

    basic setup is there just need to get the functions fully functioning

--- backend.cs
+++ backend.cs
@@ -2,33 +2,71 @@ using System;
 using System.Collections.Generic;
 
 
-namespace userShop
+namespace userShop.program
 {
     class bakeryShop
     {
-          static string _name { get; set; }
-          private int _price{ get; set; }
           
+          private int _price{ get; set; }
+            public bakeryShop()
             {
-           _name = "nothing";
-            _price = 10;
+           
+            _price = 0;
            
 
             }
 
-         public bakeryShop(string Name, int Price)
+         public bakeryShop(int breadPrice)
         {
-         _name = Name;
-         _price = Price;
+        
+         _price = breadPrice;
     }
-    }
+        // public void breadprice()
+        // {
+        // newBreadPrice *3.33;
 
-
-
-    class pastryShop
-    {
-        public static void myPastry()
+        // }
+        public int getPrice()
         {
-            
+            return _price;
         }
+
     }
+
+   class pastryShop
+    {
+          
+          private int _price{ get; set; }
+            public pastryShop()
+            {
+           
+            _price = 0;
+           
+
+            }
+
+         public pastryShop(int pastryPrice)
+        {
+        
+         _price = pastryPrice;
+         }
+        // public  pastryPrice(NewPastryPrice)
+        // {
+        //  return (NewPastryPrice* 1.66);
+
+        // }
+        public int getPrice()
+        {
+            return _price;
+        }
+
+        //  public checkOut()
+        // {
+        //     Console.Clear();
+        //      Console.WriteLine("Your total is" + newPastryPrice + newBreadPrice + ".");
+        // }
+
+    }
+
+
+}
--- main.cs
+++ main.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using userShop;
+using userShop.program;
 
 namespace program
 {
@@ -8,7 +8,8 @@ namespace program
     {
         public static void Main()
         {
-            
+             bakeryShop newBakery = new bakeryShop(0);
+             pastryShop newPastry = new pastryShop(0);
            Console.WriteLine("\nHello! welcome to Kelars bakery.");
            Console.WriteLine("We have the best bread and pastries in town!");
            Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
@@ -16,8 +17,56 @@ namespace program
            Console.WriteLine("-------------------------------------");
            Console.WriteLine("1. How many pastries you like to buy?");
             string pastryAnswer = Console.ReadLine();
-             Console.WriteLine("2. How many slices of bread would you like to buy? ");
-             string breadAnswer = Console.ReadLine();
+            int newPastryPrice = int.Parse(pastryAnswer);
+           
+
+
+        if(newPastryPrice == 0)
+        {
+           Console.WriteLine("You're missing out!");
+
+        }else if(newPastryPrice == 1)
+        {
+            Console.WriteLine("Your total for your pastry is $2");
+
+        }else if (newPastryPrice == 2)
+        {
+            Console.WriteLine("Your total for your pastry is $4");
+
         }
+        
+        else if (newPastryPrice >= 3)
+        {
+           Console.WriteLine(newBakery.pastryPrice());
+        }
+
+
+    
+        Console.WriteLine("2. How many slices of bread would you like to buy? ");
+             string breadAnswer = Console.ReadLine();
+             int newBreadPrice = int.Parse(breadAnswer);
+
+
+        if(newBreadPrice == 0)
+        {
+           Console.WriteLine("You're missing out!");
+           Main();
+
+        }else if(newBreadPrice == 1)
+        {
+            Console.WriteLine("Your total for your bread is $5");
+
+        }else if (newBreadPrice == 2)
+        {
+            Console.WriteLine("Your total for your bread is $10 but you get a third loaf for free!");
+
+        }
+
+        // else(newBreadPrice >= 3)
+        // {
+        //     newBakery.breadPrice();
+        // }
+
     }
+}
 }
